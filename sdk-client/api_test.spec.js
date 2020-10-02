9303309530b7b3d7f7a8987da1fd55d300d7374a
arturo.zuniga <arturo.zuniga@tcpip.tech>
2020-10-02 17:51:47 -0500

    Se agregan test para los recursos consulta y actualizacion en los recursos de Limites de montos

--- sdk-client/api_test.spec.js
+++ sdk-client/api_test.spec.js
@@ -1070,7 +1070,7 @@ describe("PeticionesDePagoPorCoDiApi", () => {
     });
 });
 
-describe("LmitesDeMontosApi", () => {
+describe("LmitesDeMontosApi", async () => {
 
     var instance = null;
     var outhWire4= null;
@@ -1087,6 +1087,23 @@ describe("LmitesDeMontosApi", () => {
         const authorization = await outhWire4.obtainAccessTokenAppUser(USER_KEY, SECRET_KEY,"spei_admin");
         const suscription = SUBSCRIPTION
             const response = instance.obtainConfigurationsLimits(authorization, suscription, {});
+            expect(response).not.toBe(null)
+        } catch (error) {
+
+            if(error.status !== undefined ) {
+                console.log('Error:' + error.status, ' mensaje:' + error.statusText);
+            } else {
+                console.log('Error:' + error);
+            }
+        }
+    });
+    test("updateConfigurations", async () => {
+
+        try{
+            const body =  api.UpdateConfigurationsRequestDTO;
+            const authorization = await outhWire4.obtainAccessTokenAppUser(USER_KEY, SECRET_KEY,"spei_admin");
+            const suscription = SUBSCRIPTION
+            const response = instance.updateConfigurations(body, authorization, suscription, {});
             expect(response).not.toBe(null)
         } catch (error) {
 
